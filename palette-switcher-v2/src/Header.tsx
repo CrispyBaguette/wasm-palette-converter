f3be0eb4ae95e791888ee74df160c1b49271db87
Alexandre Bruyant <alexandre@bruyant.xyz>
2024-01-16 13:09:50 +0100

    Add header

--- palette-switcher-v2/src/Header.tsx
+++ palette-switcher-v2/src/Header.tsx
@@ -1,26 +1,25 @@
-import { useLocation } from "@solidjs/router";
-import type { Component } from "solid-js"
+import type { Component } from "solid-js";
 
-
-
-type HeaderItemProps = {
-  text: string;
-  target: string;
-  active: boolean;
-}
-
-const Header: Component = () => <header>
-  <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-      <a href="/" class="flex items-center">
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Palette Switcher</span>
-      </a>
-      <div class="flex items-center lg:order-2">
-        <a href="/about" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">About</a>
+const Header: Component = () => (
+  <header>
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <a href="/" class="flex items-center">
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Palette Switcher
+          </span>
+        </a>
+        <div class="flex items-center lg:order-2">
+          <a
+            href="/about"
+            class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+          >
+            About
+          </a>
+        </div>
       </div>
-    </div>
-  </nav>
-</header>
-
+    </nav>
+  </header>
+);
 
 export default Header;
